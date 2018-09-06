102b4251b90e30eed4c2a33c081b108b69cc4e10
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-09-06 16:15:58 +0700

    Routes, Controllers, Pages, oh my!

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/", name="homepage")
+     *
+     * @return Response
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+
+
+    /**
+     * @Route("/news/{slug}")
+     *
+     * @return Response
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
+    }
+}
